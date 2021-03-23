c6e6504ceb5182c470061fe88f21abbfdf4bbdf9
Prasad Bayi <74462843+prasadbayi@users.noreply.github.com>
2021-03-23 08:46:47 +0530

    started using search api, instead of orders.

--- src/app/services/romorderapi.service.ts
+++ src/app/services/romorderapi.service.ts
@@ -10,6 +10,6 @@ export class orderApiService
     constructor(private httpclient: HttpClient) {}
     getOrders(): Observable<any> {
         //TODO change this once DNS entry made
-        return this.httpclient.get("http://3.234.98.4:8080/rom/orders");
+        return this.httpclient.get("http://3.234.98.4:8080/rom/search/*");
     }
 }
