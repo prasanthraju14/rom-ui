f7c602884b82e8e2323df7752b40c2d14e49d308
Prasad Bayi <74462843+prasadbayi@users.noreply.github.com>
2021-05-31 08:23:33 +0530

    testing with local host :)

--- src/app/services/romorderapi.service.ts
+++ src/app/services/romorderapi.service.ts
@@ -10,6 +10,7 @@ export class orderApiService
     constructor(private httpclient: HttpClient) {}
     getOrders(searchStr : String): Observable<any> {
         //TODO change this once DNS entry made
-        return this.httpclient.get("http://3.234.98.4:8080/rom/search/"+searchStr);
+        return this.httpclient.get("http://localhost:8080/rom/search/"+searchStr);
+        //return this.httpclient.get("http://3.234.98.4:8080/rom/search/"+searchStr);
     }
 }
